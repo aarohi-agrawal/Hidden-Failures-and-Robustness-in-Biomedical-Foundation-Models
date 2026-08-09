85197253e5790cd5a7fe4e5622728037e0a584fe
Isabella Harrington <isabellaharrington643@gmail.com>
2026-08-09 16:50:12 -0400

    Created phantom0 classification script and set up

---
+++ scripts/classify_phantom0.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import json
+import re
+from transformers import pipeline
+
+input_file = "annotations/week4/phantom0_annotation_input.csv"
+output_file = "annotations/week4/phantom0_annotation_input.csv"
+
